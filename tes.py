b6c8c48240ffb669a7aed749747b3593b13e1944
Yasser Parambathkandy <yarafatin@gmail.com>
2022-06-28 22:44:36 -0400

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,100 +1,91 @@
+# import sys
+# from os.path import dirname
+# sys.path.append(dirname(__file__)+'/site-packages')
+
 import cv2
+from paddleocr import PaddleOCR
+import pytesseract
 import numpy as np
-from paddleocr import PaddleOCR, draw_ocr
+import os
+
+work_dir = os.path.dirname(__file__)
+
+det_dir_template = work_dir + "/models/det/{}/{}_PP-OCRv3_det_infer"
+rec_dir_template = work_dir + "/models/rec/{}/{}_PP-OCRv3_rec_infer"
+cls_dir = work_dir + "/models/cls/ch_ppocr_mobile_v2.0_cls_infer"
+
+DEFAULT_DET_LANG = "ml"
+det_langs = ("ch", "en", DEFAULT_DET_LANG)
+DEFAULT_REC_LANG = "latin"
+rec_langs = ("ch", "en", "japan", "korean", DEFAULT_REC_LANG)
+
+TESSDATA_PATH = work_dir + "/tessdata"
 
 
-def process(img_path, lang):
-    img_path = '/mnt/d/yaraf/hqocr/data/foreign/korean/00000005.tif'
-    lang = 'korean'
+def process(gray, lang):
+    try:
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    image = cv2.imread(img_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        det_lang = lang if (lang in det_langs) else DEFAULT_DET_LANG
+        rec_lang = lang if (lang in rec_langs) else DEFAULT_REC_LANG
 
-    # sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    # sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
-    # thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    #
-    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    # close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
-    # result = 255 - close
+        ocr = PaddleOCR(
+            lang=lang,
+            use_angle_cls=False,
+            det_model_dir=det_dir_template.format(det_lang, det_lang),
+            rec_model_dir=rec_dir_template.format(rec_lang, rec_lang),
+            cls_model_dir=cls_dir,
+        )
+        result = ocr.ocr(thresh, det=True, cls=False)
 
-    # Remove horizontal
-    # horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 1))
-    # detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-    # cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    # for c in cnts:
-    #     cv2.drawContours(image, [c], -1, 0, 3)
-    #
-    # # Remove vertical
-    # vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 20))
-    # detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
-    # cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    # for c in cnts:
-    #     cv2.drawContours(image, [c], -1, 0, 3)
-
-    linek = np.zeros((11, 11), dtype=np.uint8)
-    linek[5, ...] = 1
-    x = cv2.morphologyEx(gray, cv2.MORPH_OPEN, linek, iterations=1)
-    gray -= x
-
-    cv2.imshow('result', gray)
-    cv2.waitKey()
-    result = gray
-
-    # # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
-    # # You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
-    # to switch the language model in order.
-    # need to run only once to download and load model into memory
-    # ocr = PaddleOCR(use_angle_cls=True, lang='en')
-    ocr = PaddleOCR(lang=lang)
-    result = ocr.ocr(result, det=True, cls=False)
-
-    # print(result)
-    # result.sort(key=lambda x: x[0][0][1])
-
-    # for line in result:
-    #     print(line)
-
-    myDict = {}
-    prevline = 0
-    for line in result:
-        coord = line[0]
-        key = coord[0][1]
-        xaxis = coord[0][0]
-        if prevline != 0:
-            diff = key - prevline
-            if diff <= 45:
-                myDict.setdefault(prevline, []).append((line[1][0], xaxis))
+        myDict = {}
+        prevline = 0
+        for line in result:
+            coord = line[0]
+            key = coord[0][1]
+            xaxis = coord[0][0]
+            if prevline != 0:
+                diff = key - prevline
+                if diff <= 45:
+                    myDict.setdefault(prevline, []).append((line[1][0], xaxis))
+                else:
+                    myDict.setdefault(key, []).append((line[1][0], xaxis))
+                    prevline = key
             else:
                 myDict.setdefault(key, []).append((line[1][0], xaxis))
                 prevline = key
-        else:
-            myDict.setdefault(key, []).append((line[1][0], xaxis))
-            prevline = key
 
-    with open('{}.paddle.txt'.format(img_path), 'w') as fp:
+        ocr = []
         for key in myDict:
             myDict[key].sort(key=lambda x: x[1])
-            data = ' '.join([tup[0] for tup in myDict[key]])
-            fp.write("%s\n" % data)
+            ocr.append(" ".join([tup[0] for tup in myDict[key]]))
+        return "\n".join(ocr)
+    except BaseException as err:
+        print(err)
+        return "unable to ocr"
 
-    # with open(r'out.txt', 'w') as fp:
-    #     for line in result:
-    #         # write each item on a new line
-    #         fp.write("%s\n" % line[1][0])
-    #     print('Done')
 
-    # draw result
-    # from PIL import Image
-    # image = Image.open(img_path).convert('RGB')
-    # boxes = [line[0] for line in result]
-    # txts = [line[1][0] for line in result]
-    # scores = [line[1][1] for line in result]
-    # im_show = draw_ocr(image, boxes, txts, scores, font_path='./korean.ttf')
-    # im_show = Image.fromarray(im_show)
-    # im_show.save('result.jpg')
+def initial(gray):
+    try:
+        # sharpen
+        sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+        thresh = cv2.threshold(
+            sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+        )[1]
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+        result = 255 - close
 
-process('', '')
+        # resize
+        scale_percent = 70
+        width = int(gray.shape[1] * scale_percent / 100)
+        height = int(gray.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(result, dim, interpolation=cv2.INTER_AREA)
+
+        # ocr
+        custom_config = "-l  chi_sim+kor+jpn --tessdata-dir " + TESSDATA_PATH
+        return pytesseract.image_to_string(resized, config=custom_config)
+    except BaseException as err:
+        return "error in initial ocr"
